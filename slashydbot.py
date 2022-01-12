ee88bcee7da0e2e1027df7d9c598d230a90b3479
shashwot2 <49743516+shashwot2@users.noreply.github.com>
2022-01-12 15:08:02 +0700

    Update slashydbot.py

--- slashydbot.py
+++ slashydbot.py
@@ -152,7 +152,7 @@ async def ban(ctx, member: discord.Member, *, reason="Ask the admin"):
 #TODO: BLACKJACK
 # @client.command(name="blackjack", help= "plays blackjack")
 # async def blackjack(ctx):
-#    a = {1:"A",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10",11:"J",12:"Q",13:"K"}
+#    a = {1:"A", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", 10:"10", 11:"J", 12:"Q", 13:"K"}
 #    while running = 1:
 #  
 #       dtotal = 0
